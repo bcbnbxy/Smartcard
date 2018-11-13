634ab53f808de1bed31097b4389674d7e848fa5e
liangfahua <930110543@qq.com>
2018-11-13 10:30:38 +0800

    +++++++++++++++++++++

---
+++ common/urls.js
@@ -0,0 +1,18 @@
+//根据unionid根据用户信息
+module.exports.getUserbyUnionid = 'http://localhost:8080/passport/wxuser/login/cardminiprogram';
+//获取智能名片小程序AccessToken
+module.exports.getAccessToken = 'http://localhost:8080/wechatCardmini/getAccessToken';
+//生成智能名片小程序二维码
+module.exports.getWxacodeunlimit = 'https://api.weixin.qq.com/wxa/getwxacodeunlimit';
+
+
+
+
+
+
+
+
+
+
+
+
--- utils/util.js
+++ utils/util.js
@@ -17,3 +17,22 @@ const formatNumber = n => {
 module.exports = {
   formatTime: formatTime
 }
+
+module.exports.splitParameter=function(aUrl, key, value) {
+  // return aUrl.indexOf('?') > -1 ? aUrl + '&' + key + '=' + value : aUrl + '?' + key + '=' + value;
+  // url1或url2里如果有$，再根据是否有参数做处理
+  if (aUrl.indexOf('$') > -1) {
+    if (aUrl.indexOf('=') > -1) {
+      return aUrl + '&' + key + '=' + value;
+    }
+    else {
+      return aUrl + key + '=' + value;
+    }
+  }
+  if (aUrl.indexOf('?') > -1) {
+    return aUrl + '&' + key + '=' + value;
+  }
+  if (aUrl.indexOf('$') === -1 && aUrl.indexOf('?') === -1) {
+    return aUrl + '?' + key + '=' + value;
+  }
+ }
